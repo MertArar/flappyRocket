8f4085bcd363b086aa0bf569f2556677c94ca662
MertArar <mertarar.ma@gmail.com>
2023-09-25 17:27:40 +0300

    Level Design added

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    
+    [SerializeField] float levelLoadDelay = 2f; 
     void OnCollisionEnter(Collision other) 
     {
       switch(other.gameObject.tag)
@@ -11,15 +13,40 @@ public class CollisionHandler : MonoBehaviour
             Debug.Log("This thing is friendly");
             break;
         case "Finish":
-            Debug.Log("Congrast, you finished!");
+            StartSuccessSequence();
             break;
         case "Fuel":
             Debug.Log("You picked up fuel");
             break;
         default:
-            ReloadLevel();
+            StartCrashSequence();
             break;
       }  
+    }
+    
+    void StartSuccessSequence() 
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    
+    void StartCrashSequence()
+    {
+        // todo add SFX upon crash
+        // todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+    
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1; 
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
     
     void ReloadLevel()
