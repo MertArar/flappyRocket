9b64fc37f7bed80c7f731dee8528a766617c6f9f
MertArar <mertarar.ma@gmail.com>
2023-09-28 19:38:07 +0300

    Particle added into the game

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,6 +8,10 @@ public class CollisionHandler : MonoBehaviour
     [SerializeField] AudioClip success; 
     [SerializeField] AudioClip crash; 
     
+    [SerializeField] ParticleSystem successParticles; 
+    [SerializeField] ParticleSystem crashParticles;
+    
+    
     AudioSource audioSource;
     
     bool isTransitioning = false;
@@ -46,7 +50,7 @@ public class CollisionHandler : MonoBehaviour
         isTransitioning = true;
         audioSource.Stop();
         audioSource.PlayOneShot(success);
-         // todo add particle effect upon success
+        successParticles.Play();
         GetComponent<Movement>().enabled = false;
         Invoke("LoadNextLevel", levelLoadDelay);
     }
@@ -56,7 +60,7 @@ public class CollisionHandler : MonoBehaviour
         isTransitioning = true;
         audioSource.Stop();
         audioSource.PlayOneShot(crash);
-        // todo add particle effect upon success
+        crashParticles.Play();
         GetComponent<Movement>().enabled = false;
         Invoke("ReloadLevel", levelLoadDelay);
     }
